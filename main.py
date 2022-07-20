1661e5ae2e3cc9517b7af122f58fd31dc4803606
Chris Ebert <christiangeorg.ebert@uzh.ch>
2022-07-21 00:33:12 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from translit import Transliterator
+import translit
 
 
-Transliterator = Transliterator('sin')
+def main():
+    Transliterator = translit.Transliterator('ben')
 
-print(Transliterator)
+    print(Transliterator.conversion_table['ত'])
+    ben_test = 'এটাকে অন্ধকার দেখাচ্ছে যেন ভূতুড়ে জাহাজের মত'
+    ben_transliterated = Transliterator.convert(ben_test)
+    print(ben_transliterated)
+    print(Transliterator.conversion_table['ত']['char_type'])
+    return
 
 
-
-print(Transliterator.convert('කලින්'))
-
+if __name__ == '__main__':
+    main()
