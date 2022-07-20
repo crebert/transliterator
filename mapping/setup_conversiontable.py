e9b27f0010f65c437f7820299f52825c79f870b7
Chris Ebert <christiangeorg.ebert@uzh.ch>
2022-07-20 23:10:42 +0200

    generalize for more languages

--- mapping/setup_conversiontable.py
+++ mapping/setup_conversiontable.py
@@ -2,27 +2,29 @@ import csv
 import os
 
 
-def file_finder(path, lang=''):
+def file_finder(path: str, lang: str):
     """
-    list of all files in path with given language code
-    :param path: str path
-    :param lang: str 3-digit language isocode
+    list of all monotext files in corpus with given language code
+    :param path: path name to corpus files
+    :param lang: 3-digit language isocode
     :return: list
     """
     all_files = []
     movies = [i for i in os.listdir(path) if not i.startswith('.')]
     for dirs in movies:
         local_path = os.path.join(path, dirs, 'monotext')
-        sin_files = [i for i in os.listdir(local_path) if i.endswith(lang + '.monotext.txt')]
-        files_full_path = [os.path.join(local_path, x) for x in sin_files]
+        monotext_files = [i for i in os.listdir(local_path) if i.endswith(lang + '.monotext.txt')]
+        files_full_path = [os.path.join(local_path, x) for x in monotext_files]
         all_files.extend(files_full_path)
     return all_files
 
 
 def main():
     corpus = '../../git_projects/partree/corpus'
+    # language code
+    lang = 'ben'
     # get all files in corpus
-    files = file_finder(corpus, lang='ben')
+    files = file_finder(corpus, lang=lang)
     # get all chars in all files
     chars = []
     for i in files:
@@ -30,9 +32,9 @@ def main():
         with open(i) as f:
             lines = f.readlines()
         # get text (second column in tab separated file)
-        sin_text = [x.split('\t')[1] for x in lines]
+        text = [x.split('\t')[1] for x in lines]
         # save all (unique) characters
-        for line in sin_text:
+        for line in text:
             # split into chars (only add non-ASCII chars)
             local_chars = [x for x in line if not x.isascii()]
             # add to main list
@@ -42,8 +44,8 @@ def main():
     # print(len(chars))
 
     # save to file
-    with open('ben_conversiontable.csv', 'w') as csv_file:
-        # write to file
+    with open(f"{lang}_conversiontable.csv", 'w') as csv_file:
+        # write to filse
         writer = csv.writer(csv_file)
         writer.writerow(['native', 'latin'])
         for i in chars:
