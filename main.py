cf9c5e16bd2bab010c8681b295e930d0d99b9a27
Chris Ebert <christiangeorg.ebert@uzh.ch>
2022-10-02 23:04:43 +0200

    comments

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import translit
 
 
 def main():
+    # create transliterator for Bengali (isocode)
     Transliterator = translit.Transliterator('ben')
 
+    # show Latin character
     print(Transliterator.conversion_table['ত'])
+
+    # show char_type (diacritic or non-diacritic)
+    print(Transliterator.conversion_table['ত']['char_type'])
+
+    # transliteration of a string
     ben_test = 'এটাকে অন্ধকার দেখাচ্ছে যেন ভূতুড়ে জাহাজের মত'
     ben_transliterated = Transliterator.convert(ben_test)
     print(ben_transliterated)
-    print(Transliterator.conversion_table['ত']['char_type'])
     return
 
 
